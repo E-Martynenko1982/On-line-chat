1e1749b3545eddcb62039a965e6e51c824c832eb
E-Martynenko1982 <motor8103@gmail.com>
2024-12-20 11:09:21 +0200

    update

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../styles/Main.module.css';
+
+const Main = () => {
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>
+          Join
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Main;
